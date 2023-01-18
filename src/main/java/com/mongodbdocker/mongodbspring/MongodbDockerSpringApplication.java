14f7b161d53a55b59a19fa691ece8daa2ce1de13
bneeruga <bneeruga@buffalo.edu>
2023-01-18 03:02:07 -0500

    Added Product entity

---
+++ src/main/java/com/mongodbdocker/mongodbspring/MongodbDockerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mongodbdocker.mongodbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbDockerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbDockerSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mongodbdocker/mongodbspring/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.mongodbdocker.mongodbspring.product;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
